8410d0514bc289c8587289e15452ebae17b832be
Denis Haine <denis.haine@gmail.com>
2015-04-21 21:13:59 -0400

    Add tab[1:2, 1:2].

--- R/multidimBias.R
+++ R/multidimBias.R
@@ -108,6 +108,7 @@ multidimBias <- function(exposed,
     if(inherits(exposed, c("table", "matrix")))
         tab <- exposed
     else tab <- table(exposed, case)
+    tab <- tab[1:2, 1:2]
 
     a <- tab[1, 1]
     b <- tab[1, 2]
