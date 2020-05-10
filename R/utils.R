44077ccd6d3eee210028ecc4856f512e24b327ec
Denis Haine <denis.haine@gmail.com>
2020-05-10 14:04:48 -0400

    Docs (utils) Remove callback_df from docs.

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,11 @@
-#' Given a contingency table (2-by-2) (as a data frame or matrix)
-#' it will untabulate it by repeating each row by the number of times
-#' it was repeated.
-#'
-#' @param matrix or data.frame to untable
-#' @param vector of counts (of same length as \code{df})
-#' @keywords internal
+# Given a contingency table (2-by-2) (as a data frame or matrix)
+# it will untabulate it by repeating each row by the number of times
+# it was repeated.
+#
+# @param matrix or data.frame to untable
+# @param vector of counts (of same length as \code{df})
+# @keywords internal
 callback_df <- function(df, num) {
     df[rep(1:nrow(df), num), ]
 }
+
