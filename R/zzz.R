b82fb8b3697ad422c6c214898930781ca4bb729b
Denis Haine <denis.haine@gmail.com>
2023-08-29 14:43:34 -0400

    fix: (startup) Add utils to namespace.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
+#' @importFrom utils packageVersion
 .onAttach <- function(libname, pkgname) {
-    message("Thank you for using episensr!")
+    packageStartupMessage("Thank you for using episensr!")
     packageStartupMessage("This is version ", packageVersion(pkgname), " of ", pkgname)
     packageStartupMessage("Type 'citation(\"episensr\")' for citing this R package in publications.\n")
 }
