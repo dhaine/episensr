5eb79bcea9bc38bafa44a12e0bdc275e918194d4
Denis Haine <denis.haine@gmail.com>
2024-05-22 14:35:12 -0400

    clean: (style) Correct lintr for plot.mbias.

--- R/plot.mbias.R
+++ R/plot.mbias.R
@@ -15,20 +15,20 @@
 #' @examples
 #' plot(mbias(or = c(2, 5.4, 2.5, 1.5, 1),
 #' var = c("HIV", "Circumcision", "Muslim", "Low CD4", "Participation")))
-#' 
+#'
 #' @export
 #' @importFrom ggplot2 ggplot aes fortify annotate theme theme_bw
 #' @importFrom dagitty coordinates
 #' @importFrom ggdag dagify coords2df coords2list m_bias confounder_triangle ggdag theme_dag
 plot.mbias <- function(x,
                        type = c("before", "after"),
-                       dec = 2, 
+                       dec = 2,
                        ...) {
     obj <- x
     type <- match.arg(type)
-    
-    if(type == "before") {
-        coords <- dagitty::coordinates(m_bias()) %>% 
+
+    if (type == "before") {
+        coords <- dagitty::coordinates(m_bias()) %>%
             coords2df()
 
         .dag <- dagify(x ~ a,
@@ -36,10 +36,10 @@ plot.mbias <- function(x,
                        y ~ x + b,
                        exposure = "x",
                        outcome = "y",
-                       labels = c(x = obj[[5]][2], 
-                                  y = obj[[5]][1], 
-                                  a = obj[[5]][3], 
-                                  b = obj[[5]][4], 
+                       labels = c(x = obj[[5]][2],
+                                  y = obj[[5]][1],
+                                  a = obj[[5]][3],
+                                  b = obj[[5]][4],
                                   m = obj[[5]][5]),
                        coords = coords2list(coords))
 
@@ -48,7 +48,7 @@ plot.mbias <- function(x,
             annotate("text", x = .1, y = .5,
                      label = round(obj[[4]][1], dec)) +
             annotate("text", x = .4, y = .7,
-                     label = round(obj[[4]][2], dec)) + 
+                     label = round(obj[[4]][2], dec)) +
         annotate("text", x = 1.6, y = .7,
                  label = round(obj[[4]][3], dec)) +
         annotate("text", x = 1.9, y = .5,
