1079b293c560245eccce3f6c4357362f2f480f53
Denis Haine <denis.haine@gmail.com>
2024-08-08 15:47:45 -0400

    update: (pipe)

--- R/pipe.R
+++ R/pipe.R
@@ -10,15 +10,15 @@
 #' @param lhs,rhs Data or bias function and a function to apply to it
 #' @examples
 #' # Instead of
-#' misclassification(matrix(c(118, 832, 103, 884),
+#' misclass(matrix(c(118, 832, 103, 884),
 #' dimnames = list(c("BC+", "BC-"), c("AD+", "AD-")), nrow = 2, byrow = TRUE),
 #' type = "exposure", bias_parms = c(.56, .58, .99, .97))
 #' # you can write
 #' dat <- matrix(c(118, 832, 103, 884),
 #' dimnames = list(c("BC+", "BC-"), c("AD+", "AD-")), nrow = 2, byrow = TRUE)
-#' dat %>% misclassification(., type = "exposure", bias_parms = c(.56, .58, .99, .97))
+#' dat %>% misclass(., type = "exposure", bias_parms = c(.56, .58, .99, .97))
 #' # also for multiple bias:
 #' dat %>%
-#' misclassification(., type = "exposure", bias_parms = c(.56, .58, .99, .97)) %>%
+#' misclass(., type = "exposure", bias_parms = c(.56, .58, .99, .97)) %>%
 #' multiple.bias(., bias_function = "selection", bias_parms = c(.73, .61, .82, .76))
 NULL
