7cd9e50f07c15c30fe100459f13f1629dffebfcb
Denis Haine <denis.haine@gmail.com>
2024-08-12 19:26:57 -0400

    update: (multiple) Remove multiple.bias().

--- R/pipe.R
+++ R/pipe.R
@@ -17,8 +17,4 @@
 #' dat <- matrix(c(118, 832, 103, 884),
 #' dimnames = list(c("BC+", "BC-"), c("AD+", "AD-")), nrow = 2, byrow = TRUE)
 #' dat %>% misclass(., type = "exposure", bias_parms = c(.56, .58, .99, .97))
-#' # also for multiple bias:
-#' dat %>%
-#' misclass(., type = "exposure", bias_parms = c(.56, .58, .99, .97)) %>%
-#' multiple.bias(., bias_function = "selection", bias_parms = c(.73, .61, .82, .76))
 NULL
