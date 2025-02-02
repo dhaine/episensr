3cab6e3e7856fef3cd8322ac5465d230c4ead153
Denis Haine <denis.haine@gmail.com>
2025-02-02 14:10:35 -0500

    add: (perf) Update binom calc to armadillo.

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 cpprbinom <- function(n, size, prob) {
-    .Call('_episensr_cpprbinom', PACKAGE = 'episensr', n, size, prob)
+    .Call(`_episensr_cpprbinom`, n, size, prob)
 }
 
--- src/misclass_record.cpp
+++ src/misclass_record.cpp
@@ -1,9 +1,11 @@
-#include <Rcpp.h>
+#include <RcppArmadillo.h>
 using namespace Rcpp;
 
+// [[Rcpp::depends(RcppArmadillo)]]
+
 // [[Rcpp::export]]
-NumericVector cpprbinom(int n, double size, NumericVector prob) {
-  NumericVector v = no_init(n);
+NumericVector cpprbinom(int n, double size, arma::vec prob) {
+  arma::vec v = arma::vec(n);
   std::transform(prob.begin(), prob.end(), v.begin(), [=](double p){return R::rbinom(size, p);});
-  return(v);
+  return(wrap(v));
 }
